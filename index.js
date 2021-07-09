0ab44a59072edd4b8434810c8bd7fa50e57bcb70
codenameransom <codejoshuaroque@gmail.com>
2021-07-09 10:42:52 -0400

    commit tree d1db029c5f01067da35899b09c26d4d4ae759510

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+
+function writeCards(nameArrays, eName){
+    let cards = [];
+  for (let i = 0; i < nameArrays.length; i++) {
+      cards.push(`Thank you, ${nameArrays[i]}, for the wonderful ${eName} gift!`);
+  }
+
+    return cards;
+}
+
+function countDown(integer){
+    for (let i = integer; i >= 0; i-- ) {
+        console.log(i)
+    }   
+}
